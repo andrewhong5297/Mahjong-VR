99a773067b5631808a3677e80274a8ab27da82bc
andrewhong5297 <47720952+andrewhong5297@users.noreply.github.com>
2020-04-26 14:49:51 -0400

    tried refactoring turnmanager file

---
+++ MahjongVR/Assets/scripts/UpdateBoxManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateBoxManager : MonoBehaviour
+{
+    public Scrollbar bar;
+    public IEnumerator ScrollToBottom()
+    {
+        yield return new WaitForEndOfFrame();
+        Debug.Log("scrolling to bottom");
+        bar.value = 0f;
+    }
+}
